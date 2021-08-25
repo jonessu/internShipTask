54c489746dcde01ecbc8f91751abd8ebf9eb8a99
Jones9601 <jonessunil9601@gmail.com>
2021-08-25 23:09:44 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
         supportedLocales: Locales.supportedLocales,
         locale: locale,
         //home: LOGINFORMSCREEN(),
-        home: OfflineStorageScreen(),
+        home: LoginformScreen(),
       ),
     );
   }
